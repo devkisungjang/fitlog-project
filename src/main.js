981c3848fc0bab5681d31bc9df14c6b514ce0348
kisung <jjangki2071@gmail.com>
2024-07-29 09:16:30 +0900

    pinia 설치 후 임포트

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 // Plugins
 import { registerPlugins } from "@/plugins";
+import { createPinia } from "pinia";
 
 // Components
 import App from "./App.vue";
@@ -10,6 +11,7 @@ import { useKakao } from "vue3-kakao-maps";
 import { createApp } from "vue";
 
 const app = createApp(App);
+const pinia = createPinia();
 
 useKakao("5bd7be4085eeed06609b9a85cfba8add", [
   "clusterer",
@@ -19,4 +21,5 @@ useKakao("5bd7be4085eeed06609b9a85cfba8add", [
 registerPlugins(app);
 
 app.use(router);
+app.use(pinia);
 app.mount("#app");
