043a11add44441468503b55e14a88b941159e2a6
kisung <jjangki2071@gmail.com>
2024-07-31 12:19:30 +0900

    종목 선택 페이지 디자인 중심 리펙토링 완료

--- src/pages/exercise_selection.vue
+++ src/pages/exercise_selection.vue
@@ -3,7 +3,7 @@
   <v-card>
     <v-tabs v-model="tab" bg-color="#0099F7" slider-color="#ffff" show-arrows>
       <v-tab
-        v-for="(exercises, group) in exerciseGroups"
+        v-for="(exercise, group) in exerciseGroups"
         :key="group"
         :value="group"
         class="tab"
@@ -16,7 +16,8 @@
           <div
             v-for="(exercise, index) in exerciseGroups[tab]"
             :key="exercise.id"
-            class="exercise-item"
+            :class="['exercise-item', { selected: checkedItems[tab][index] }]"
+            @click="toggleSelect(tab, index)"
           >
             <v-list-item-avatar v-if="exercise.image">
               <v-img :src="exercise.image" class="exercise-image" />
@@ -26,8 +27,12 @@
                 exercise.name
               }}</v-list-item-title>
             </v-list-item-content>
-            <v-checkbox v-model="checkedItems[tab][index]" class="checkbox">
-            </v-checkbox>
+            <v-icon
+              :color="checkedItems[tab][index] ? 'green' : 'grey'"
+              class="check-icon"
+            >
+              mdi-check-circle
+            </v-icon>
           </div>
         </v-tab-item>
       </v-tabs-items>
@@ -59,18 +64,24 @@ export default {
     }, {}),
   }),
   methods: {
+    toggleSelect(group, index) {
+      this.checkedItems[group].splice(
+        index,
+        1,
+        !this.checkedItems[group][index]
+      );
+    },
     startExercise() {
       const fitlogStore = useFitlogStore();
       const selectedExercises = [];
 
-      Object.keys(this.checkedItems).forEach((group, groupIndex) => {
+      Object.keys(this.checkedItems).forEach((group) => {
         this.checkedItems[group].forEach((isChecked, index) => {
           if (isChecked) {
             selectedExercises.push(this.exerciseGroups[group][index].name);
           }
         });
       });
-
       fitlogStore.setSelectedExercises(selectedExercises);
     },
   },
@@ -89,31 +100,50 @@ export default {
   border-radius: 20px;
   background-color: white;
   box-shadow: 0 10px 20px rgba(0, 0, 0, 0.19), 0 6px 6px rgba(0, 0, 0, 0.23);
+  cursor: pointer;
+  transition: background-color 0.3s ease, transform 0.3s ease;
 }
+
+.exercise-item.selected {
+  background-color: #cceeff; /* 청록색 계열의 밝은 색상 */
+  transform: scale(1.02);
+}
+
 .exercise-image {
   height: 120px;
   width: 120px;
   object-fit: cover;
   margin-right: 10px;
 }
+
+.exercise-title {
+  flex: 1;
+  font-size: 20px;
+  font-weight: 700;
+  text-align: center;
+}
+
+.check-icon {
+  font-size: 36px;
+  margin-left: 20px;
+  margin-right: 20px;
+  width: 36px;
+  height: 36px;
+}
+
 .v-card-text {
   padding: 10px 10px;
   background-color: #f3f4f6;
 }
-.exercise-title {
-  font-size: 20px;
-  font-weight: 700;
-}
+
 .recordBtn {
   width: 100%;
   background: #0099f7;
   color: white;
   font-weight: 700;
 }
+
 .tab {
   font-size: 16px;
-}
-.checkbox {
-  padding-top: 20px;
 }
 </style>
